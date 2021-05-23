87124adc08eb66ced591a171d399e7d843aa043e
sachinchouksey017 <sachinchouksey017@gmail.com>
2021-05-24 00:24:48 +0530

    Added app component

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [AppComponent],
+    providers: [],
+})
+export class AppModule { }
