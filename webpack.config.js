38f3122cc9e31c7b8fe3c90e9ea19735b49afcfe
sachinchouksey017 <sachinchouksey017@gmail.com>
2021-05-23 21:10:46 +0530

    Added html webpack plugin for auto genrate html also added style loader and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: './js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
+    mode: 'production',
+    entry: './js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        clean:true
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Development',
+          template:'index.html'
+        }),
+      ],
+    devServer: {
+        contentBase: "./dist",
+        // hot: true,
+    }
 };
