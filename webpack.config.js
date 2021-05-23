0fd24b989a80ac1a8a52bfa44122182c7d731ac9
sachinchouksey017 <sachinchouksey017@gmail.com>
2021-05-24 00:55:40 +0530

    Added bable loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack');
 
 module.exports = {
     mode: 'production',
-    entry: './src/js/index.js',
+    entry: {
+        polyfills: './src/polyfills.ts',
+        vendor: './src/vendor.ts',
+        app: './src/main.ts'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            process: "process/browser"
+         } 
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js',
-        clean:true
+        publicPath: '/',
+        filename: '[name].bundle.js',
+        chunkFilename: '[id].chunk.js'
     },
+    
     module:{
         rules:[
+            {
+                test: /\.ts$/,
+                use: [
+                    'babel-loader',
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: {
+                            configFileName: path.resolve(__dirname, 'tsconfig.json')
+                        }
+                    },
+                    'angular2-template-loader'
+                ],
+                exclude: [/node_modules/]
+            },
             {
                 test:/\.css$/,
                 use:[
@@ -29,9 +56,17 @@ module.exports = {
           title: 'Development',
           template:'src/index.html'
         }),
+        new webpack.optimize.SplitChunksPlugin({
+            name: ['app', 'vendor', 'polyfill']
+        }),
       ],
     devServer: {
         contentBase: "./dist",
         // hot: true,
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
     }
 };
